92bf3d221267245fc1dab3f1be99a304d00a0e3f
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2024-11-21 19:02:15 +0100

    created page structure

--- src/components/jsx/routes/index.jsx
+++ src/components/jsx/routes/index.jsx
@@ -1,11 +1,28 @@
 import { Routes, Route } from "react-router-dom";
+import Layout from "../layout";
+import Home from "../pages/home";
+import Venue from "../pages/venue";
+import About from "../pages/about";
+import Contact from "../pages/contact";
+import Register from "../pages/register";
+import Login from "../pages/login";
+import Profile from "../pages/profile";
+import RouteNotFound from "../pages/routeNotFound";
 
 function AppRoutes() {
   return (
     <div>
       <Routes>
-        <Route index element={<div className="App">Page under construction</div>} />
-        <Route path="*" element={<div>Route not found</div>} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/:id" element={<Venue />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<RouteNotFound />} />
+        </Route>
       </Routes>
     </div>
   )
