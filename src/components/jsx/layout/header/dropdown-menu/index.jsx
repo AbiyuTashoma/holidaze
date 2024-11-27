7f3907b23c395420632d4b3c3b242710dff506d0
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2024-11-27 18:48:03 +0100

    improved dropdown link and  menu style

--- src/components/jsx/layout/header/dropdown-menu/index.jsx
+++ src/components/jsx/layout/header/dropdown-menu/index.jsx
@@ -1,15 +1,16 @@
 import { Dropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function NavMenu() {
   return (
     <Dropdown menuVariant="dark">
       <Dropdown.Toggle id="dropdown-basic">Menu</Dropdown.Toggle>
-      <Dropdown.Menu>
-        <Dropdown.Item href="/">Home</Dropdown.Item>
-        <Dropdown.Item href="/login">Login</Dropdown.Item>
-        <Dropdown.Item href="/register">Register</Dropdown.Item>
-        <Dropdown.Item href="/contact">Contact us</Dropdown.Item>
-        <Dropdown.Item href="/about">About us</Dropdown.Item>
+      <Dropdown.Menu align="end">
+        <Dropdown.Item className='p-0'><Link className='dropdown-item' to="/">Home</Link></Dropdown.Item>
+        <Dropdown.Item className='p-0'><Link className='dropdown-item' to="/login">Login</Link></Dropdown.Item>
+        <Dropdown.Item className='p-0'><Link className='dropdown-item' to="/register">Register</Link></Dropdown.Item>
+        <Dropdown.Item className='p-0'><Link className='dropdown-item' to="/contact">Contact us</Link></Dropdown.Item>
+        <Dropdown.Item className='p-0'><Link className='dropdown-item' to="/about">About us</Link></Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   )
