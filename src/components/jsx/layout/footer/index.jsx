65f6c68528da38d1e336cb50aa8830460dd973d8
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2024-11-30 08:13:54 +0100

    updated footer style

--- src/components/jsx/layout/footer/index.jsx
+++ src/components/jsx/layout/footer/index.jsx
@@ -5,12 +5,12 @@ function Footer() {
     return (
       <footer className="text-center py-3">
         <div>&#169; Holidaze 2025</div>
-        <ul className="footer-elements">
-            <li><Link className="footer-link" to="/">Home</Link></li>
+        <ul className="footer-elements d-flex justify-content-between">
+            <li><Link className="footer-link ms-1" to="/">Home</Link></li>
             <li><Link className="footer-link" to="/login">Login</Link></li>
             <li><Link className="footer-link" to="/register">Register</Link></li>
             <li><Link className="footer-link" to="/contact">Contact us</Link></li>
-            <li><Link className="footer-link" to="/about">About us</Link></li>
+            <li><Link className="footer-link me-1" to="/about">About us</Link></li>
         </ul>
         <div className="d-flex justify-content-center">
           <p className="m-0">Follow us on:</p>
