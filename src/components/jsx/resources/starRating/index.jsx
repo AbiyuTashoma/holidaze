a0679a384d82c13494d97efe420cef21d3aa7cc9
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2024-12-07 18:38:29 +0100

    added venue rating

---
+++ src/components/jsx/resources/starRating/index.jsx
@@ -0,0 +1,76 @@
+function StarRating (rating) {
+    switch(rating) {
+        case 0:
+            return (
+                <div className="rating">
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
+                </div>
+            );
+        case 1:
+            return (
+                <div className="rating">
+                    <span className="fa fa-star review-checked"></span>
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
+                </div>
+            );
+        case 2:
+            return (
+                <div className="rating">
+                    <span className="fa fa-star review-checked"></span>
+                    <span className="fa fa-star review-checked"></span>
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
+                </div>
+            );
+        case 3:
+            return (
+                <div className="rating">
+                    <span className="fa fa-star review-checked"></span>
+                    <span className="fa fa-star review-checked"></span>
+                    <span className="fa fa-star review-checked"></span>
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
+                </div>
+            );
+        case 4:
+            return (
+                <div className="rating">
+                    <span className="fa fa-star review-checked"></span>
+                    <span className="fa fa-star review-checked"></span>
+                    <span className="fa fa-star review-checked"></span>
+                    <span className="fa fa-star review-checked"></span>
+                    <span className="fa fa-star"></span>
+                </div>
+            );
+        case 5:
+            return (
+                <div className="rating">
+                    <span className="fa fa-star review-checked"></span>
+                    <span className="fa fa-star review-checked"></span>
+                    <span className="fa fa-star review-checked"></span>
+                    <span className="fa fa-star review-checked"></span>
+                    <span className="fa fa-star review-checked"></span>
+                </div>
+            );
+        default:
+            return (
+                <div className="rating">
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
+                    <span className="fa fa-star"></span>
+                </div>
+            );
+    }
+}
+
+export default StarRating;
--- src/components/jsx/resources/venueList/index.jsx
+++ src/components/jsx/resources/venueList/index.jsx
@@ -2,14 +2,16 @@ import { Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { currency, unit } from "../../../js/constants";
 import Facility from "../facility";
+import StarRating from "../starRating";
 
 function VenueList(venues) {
   return (
     <Container>
       <Row xs="1" md="2" xl="3">        
         {venues.map((venue) =>
-          <Col key={venue.id} className="my-2">
+          <Col key={venue.id} className="position-relative my-2">
             <Link to={'/' + venue.id}>
+              {StarRating(venue['rating'])}
               {venue['media'][0] ? <img src= {venue['media'][0]['url']} className="list-image" alt=""/> : <img src= "https://st4.depositphotos.com/17828278/24401/v/600/depositphotos_244011872-stock-illustration-image-vector-symbol-missing-available.jpg" className="list-image" alt="Not available"/>}
             </Link>
             <Row className="my-2">
