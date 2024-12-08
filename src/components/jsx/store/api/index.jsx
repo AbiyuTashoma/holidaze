93faab53de137087dbbf061aabae7a9f88698298
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2024-12-08 11:16:20 +0100

    implemented search state

--- src/components/jsx/store/api/index.jsx
+++ src/components/jsx/store/api/index.jsx
@@ -5,9 +5,11 @@ const useApi = create(persist((set) => ({
   venues: [],
   isLoading: false,
   isError: false,
+  searchText: "",
   updateVenues: (items) => set({venues: [...items]}),
   updateIsLoading: (bool) => set({isLoading: bool}),
   updateIsError: (bool) => set({isError: bool}),
+  updateSearchText: (text) => set({searchText: text}),
 }), {
   name: "apiData",
 }));
