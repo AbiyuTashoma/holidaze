cd9863e270531e4859577b7900ea4f3d880d4d01
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2024-12-12 21:14:43 +0100

    added calender - wip

--- src/components/jsx/resources/aVenueDisplay/datePicker.jsx
+++ src/components/jsx/resources/aVenueDisplay/datePicker.jsx
@@ -1,30 +1,27 @@
 import DatePicker from "react-datepicker";
 import { useState } from "react";
-import { addDays, subDays } from "date-fns";
+import { addDays } from "date-fns";
 
-// const bookingArray = [{ id: "73beeeee-eb10-4c21-a850-5e1436a8ec9c", dateFrom: "2024-12-08T23:00:00.000Z", dateTo: "2024-12-10T23:00:00.000Z" },
-//   { id: "57c8f59b-5d35-4cfc-a83f-ea4bc4ed15d5", dateFrom: "2025-02-13T22:00:00.000Z", dateTo: "2025-02-22T21:59:59.999Z" },
-//   { id: "1f67857c-494a-4ec0-a514-3500ccf1464d", dateFrom: "2024-12-08T23:00:00.000Z", dateTo: "2024-12-18T23:00:00.000Z" },
-//   { id: "5652db32-34e6-4902-b17b-3c498fa76000", dateFrom: "2024-12-24T16:21:35.029Z", dateTo: "2024-12-26T16:21:35.029Z" }];
+const bk = [{ id: "73beeeee-eb10-4c21-a850-5e1436a8ec9c", dateFrom: "2024-12-08", dateTo: "2024-12-10" },
+  { id: "57c8f59b-5d35-4cfc-a83f-ea4bc4ed15d5", dateFrom: "2025-02-13T22:00:00.000Z", dateTo: "2025-02-22T21:59:59.999Z" },
+  { id: "1f67857c-494a-4ec0-a514-3500ccf1464d", dateFrom: "2024-12-08", dateTo: "2024-12-18" },
+  { id: "5652db32-34e6-4902-b17b-3c498fa76000", dateFrom: "2024-12-24T16:21:35.029Z", dateTo: "2024-12-26T16:21:35.029Z" }];
+
+const newBooking = [
+  { start: "2024-12-06T23:00:00.000Z", end: "2024-12-08T22:59:59.999Z" },
+  { start: "2024-12-02T23:00:00.000Z", end: "2024-12-04T22:59:59.999Z" }]
 
 const bookingEmpty = [];
 
-
-// function getBooking(bkng) {
-//   let booking = [];
-//   bkng.map((item) => (booking.push({start: item["dateFrom"], end: item["dateTo"]})));
-//   console.log(booking);
-//   // return booking;
-// }
-
-function Calendar(int) {
-  const [dateRange, setDateRange] = useState([new Date(), addDays(new Date(), 7)]);
+function Calendar(bArray) {
+  const [dateRange, setDateRange] = useState([new Date(), addDays(new Date(), 3)]);
   const [startDate, endDate] = dateRange;
 
   const excludes = [
         { start: addDays(new Date(), 6), end: addDays(new Date(), 7) },
         { start: addDays(new Date(), 9), end: addDays(new Date(), 15) }
       ];
+
   return (
     <DatePicker
       dateFormat='dd/MM/yyyy'
@@ -32,9 +29,8 @@ function Calendar(int) {
       // showIcon
       onCalendarClose={() => console.log("closed")}
       // excludeDates={[addDays(new Date(), 1), {date: addDays(new Date(), 5), message: "booked"}]}
-      excludeDateIntervals={int}
+      // excludeDateIntervals={bArray}
       // highlightDates={[subDays(new Date(), 7), addDays(new Date(), 7)]}
-      // shouldCloseOnSelect={false}
       startDate={startDate}
       endDate={endDate}
       minDate={new Date()}
@@ -44,8 +40,6 @@ function Calendar(int) {
       }}
       // selectsDisabledDaysInRange={false}
       showDisabledMonthNavigation
-      // isClearable={true}
-      // inline
     />
   );
 };
--- src/components/jsx/resources/aVenueDisplay/index.jsx
+++ src/components/jsx/resources/aVenueDisplay/index.jsx
@@ -3,23 +3,11 @@ import { currency, unit } from "../../../js/constants";
 import Facility from "../facility";
 import StarRating from "../starRating";
 import Calendar from "./datePicker";
-
-const bookingArray = [{ id: "73beeeee-eb10-4c21-a850-5e1436a8ec9c", dateFrom: "2024-12-08T23:00:00.000Z", dateTo: "2024-12-10T23:00:00.000Z" },
-  { id: "57c8f59b-5d35-4cfc-a83f-ea4bc4ed15d5", dateFrom: "2025-02-13T22:00:00.000Z", dateTo: "2025-02-22T21:59:59.999Z" },
-  { id: "1f67857c-494a-4ec0-a514-3500ccf1464d", dateFrom: "2024-12-08T23:00:00.000Z", dateTo: "2024-12-18T23:00:00.000Z" },
-  { id: "5652db32-34e6-4902-b17b-3c498fa76000", dateFrom: "2024-12-24T16:21:35.029Z", dateTo: "2024-12-26T16:21:35.029Z" }];
-
-  const booking = [];
-
-
-function getBooking(bkng) {
-  let booking = [];
-  bkng.map((item) => (booking.push({start: item["dateFrom"], end: item["dateTo"]})));
-  console.log(booking);
-  return booking;
-}
+import getBooking from "../../../js/getBooking";
 
 function DisplayVenue(aVenue) {
+  const bkng = getBooking(aVenue['bookings']);
+
   return (
     <Container>
       <Row xs="1" md="2">        
@@ -32,8 +20,10 @@ function DisplayVenue(aVenue) {
           <div>{aVenue['price']} {currency}<span className="unit">{unit}</span></div>
           <div>{aVenue['description']}</div>
           {Facility(aVenue['meta'])}
-          {Calendar(getBooking(bookingArray))}
-          {/* {getBooking(bookingArray)} */}
+          <Calendar bArray={bkng}/>
+          {/* {Calendar(bkng)} */}
+          {/* {Calendar(getBooking(bookingArray))} */}
+          {/* {getBooking(aVenue['bookings']).map((item) => <div key={item['end']}>{item['start']}</div>)} */}
         </Col>
       </Row>
     </Container>
