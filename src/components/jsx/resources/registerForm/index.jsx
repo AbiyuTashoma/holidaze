68c14e1af687737955c4fc87a835b5eed892de1d
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2024-12-14 15:36:53 +0100

    updated default avatar

--- src/components/jsx/resources/registerForm/index.jsx
+++ src/components/jsx/resources/registerForm/index.jsx
@@ -2,31 +2,35 @@ import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { Button } from 'react-bootstrap';
+import { defaultAvatar, registerUrl } from '../../../js/constants';
 
 const schema = yup
   .object({
-    fullName: yup
+    name: yup
       .string()
       .matches(/^[a-zA-Z0-9_]*$/, {message: 'Only a-z A-Z 0-9 and _ characters are allowed'})
-      .min(3, 'Full name should be at least 3 characters.')
-      .required('Please enter your full name'),
+      .min(3, 'Name should be at least 3 characters.')
+      .required('Please enter your name'),
     email: yup
       .string()
       .email()
       .matches(/^[\w-.]+@stud.noroff.no/, {message: 'Enter a proper email address'})
-      .required('Please enter your email'),
+      .required('Enter your email'),
     password: yup
       .string()
       .min(8, 'Password should be at least 8 characters.')
-      .required('Please enter your password'),
+      .required('Enter a password'),
     confirmPassword: yup
       .string()
       .min(8, 'Password should be at least 8 characters.')
       .oneOf([yup.ref('password')], 'Passwords must match')
-      .required('Please enter your password'),
+      .required('Re-type your password'),
     avatar: yup
       .string()
-      .matches(/^(http(s)?:\/\/.)[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)$/, {message: 'Enter a valid url', excludeEmptyString: true})
+      .matches(/^(http(s)?:\/\/.)[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)$/, {message: 'Enter a valid url', excludeEmptyString: true}),
+    venueManager: yup
+      .string()
+      .required('Select a role')
   })
   .required();
 
@@ -40,6 +44,26 @@ function RegisterForm () {
   });
 
   function onSubmit(data, event) {
+    const registerData = {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      venueManager: data.venueManager==="true" ? true: false,
+      avatar: {url:data.avatar != "" ? data.avatar : defaultAvatar, alt:"avatar"},
+    };
+
+    const registerOption = {
+      method: "POST",
+      body: JSON.stringify(registerData),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    };
+
+    // const registerResponse = api(registerUrl, registerOption);
+
+    // console.log(registerResponse);
+    console.log(registerData);
     console.log(data);
     event.target.reset();
   }
@@ -48,9 +72,9 @@ function RegisterForm () {
     <form onSubmit={handleSubmit(onSubmit)} className='form-size'>
       <h1>Register</h1>
       <div>
-        <label htmlFor="fullName" className='form-label'>Full name</label>
-        <input id='fullName' name='fullName' className='form-control' {...register('fullName')} />
-        <p className='text-danger'>{errors.fullName?.message}</p>
+        <label htmlFor="name" className='form-label'>Full name</label>
+        <input id='name' name='name' className='form-control' {...register('name')} />
+        <p className='text-danger'>{errors.name?.message}</p>
       </div>
       <div>
         <label htmlFor="email" className='form-label'>Email</label>
@@ -66,6 +90,15 @@ function RegisterForm () {
         <label htmlFor="confirmPassword" className='form-label'>Confirm password</label>
         <input type='password' id='confirmPassword' name='confirmPassword' className='form-control' {...register('confirmPassword')} />
         <p className='text-danger'>{errors.confirmPassword?.message}</p>
+      </div>
+      <div>
+        <label htmlFor="venueManager" className='form-label'>Select role</label>
+        <select id='venueManager' name='venueManager' className='form-control' {...register('venueManager')}>
+          <option value="" default className='fst-italic'>Select role</option>
+          <option value={false}>User</option>
+          <option value={true}>Manager</option>
+        </select>
+        <p className='text-danger'>{errors.venueManager?.message}</p>
       </div>
       <div>
         <label htmlFor="avatar" className='form-label fst-italic'>Avatar url (optional)</label>
