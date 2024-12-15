4e52abd1fd38f6763e82a06fb83202158aeebcb0
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2024-12-15 03:12:12 +0100

    refactore api, validation and feedback

---
+++ src/components/js/contactValidation/index.js
@@ -0,0 +1,27 @@
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    name: yup
+      .string()
+      .min(3, "Name should be at least 3 characters.")
+      .required("Enter your full name"),
+    email: yup
+      .string()
+      .email()
+      .matches(/^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/, {
+        message: "Enter a proper email address",
+      })
+      .required("Enter your email"),
+    subject: yup
+      .string()
+      .min(3, "Subject should be at least 3 characters.")
+      .required("Enter your subject"),
+    message: yup
+      .string()
+      .min(10, "Message should be at least 10 characters.")
+      .required("Please enter your message"),
+  })
+  .required();
+
+export default schema;
