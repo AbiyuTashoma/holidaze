941c7cff83314e7f959c48c94e6997226bf0a09b
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2024-12-26 23:33:47 +0100

    delete a booking

--- src/components/jsx/resources/displayBookings/displayBookings.jsx
+++ src/components/jsx/resources/displayBookings/displayBookings.jsx
@@ -1,25 +1,43 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import StarRating from "../starRating";
+import reRoute from "../../../js/reRoute/reRoute";
+import { bookingsUrl } from "../../../js/constants";
 
-function DisplayBookings (bookingsList) {
+function DisplayBookings (bookingsList, accessToken, apiKey) {
+  async function handleDelete(id) {
+    console.log("Delete:" + id);
+    const deleteOption = {
+      method: "DELETE",
+      body: JSON.stringify({}),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        Authorization: `Bearer ${accessToken}`,
+        "X-Noroff-API-Key": apiKey.data.key,
+      },
+    };
+
+    await fetch(bookingsUrl + '/' + id, deleteOption);
+    setTimeout(reRoute(window.location.pathname), 1500);
+  }
   return (
     <Container className="mb-4">
       <Row xs="1" sm="2" md="3" lg="4">
         {bookingsList.map((booking) => 
-          <Col className="mb-3">
+          <Col className="mb-3" key={booking.id}>
             <Col className="position-relative">
               {booking["venue"]['media'][0] ? <img src= {booking["venue"]['media'][0]['url']} className="list-image" alt=""/> : <img src= "https://st4.depositphotos.com/17828278/24401/v/600/depositphotos_244011872-stock-illustration-image-vector-symbol-missing-available.jpg" className="list-image" alt="Not available"/>}
               {StarRating(booking["venue"]['rating'])}
             </Col>
-            <Col key={booking.id}>
+            <Col>
               <div className="pt-2">
                 <div>{booking["venue"]["name"]}</div>
                 <div>Date in: {(new Date(booking["dateFrom"])).toDateString()}</div>
                 <div>Date out: {(new Date(booking["dateTo"])).toDateString()}</div>
                 <div>Guests: {booking["guests"]}</div>
                 <div className="my-2">
-                  <Link to={'/' + booking["venue"].id} className="btn btn-primary">View venue</Link>
+                  <Button href={'/' + booking["venue"].id} className="btn btn-primary" size="sm">View venue</Button>
+                  <Button className="ms-2" variant="secondary" onClick={() => handleDelete(booking.id)} size="sm">Delete booking</Button>
                 </div>
               </div>
             </Col>
