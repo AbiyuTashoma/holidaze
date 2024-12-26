44a2f1cdbedfa014574206724ae425454ea3257e
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2024-12-26 22:39:15 +0100

    display bookings list for a profile

--- src/components/jsx/resources/myBookings/index.jsx
+++ src/components/jsx/resources/myBookings/index.jsx
@@ -6,41 +6,6 @@ import useUser from "../../store/user";
 import { shallow } from "zustand/shallow";
 import DisplayBookings from "../displayBookings/displayBookings";
 
-const bookingsList = [
-  {
-    "id": "553f496e-dd37-4c41-9d74-e1ac393cc117",
-    "dateFrom": "2025-04-03T22:00:00.000Z",
-    "dateTo": "2025-04-12T21:59:59.999Z",
-    "guests": 1,
-    "created": "2024-11-07T08:21:51.492Z",
-    "updated": "2024-11-07T08:21:51.492Z"
-  },
-  {
-    "id": "566d27e0-8fae-42d5-a34d-5a92a4f37ccb",
-    "dateFrom": "2025-05-03T22:00:00.000Z",
-    "dateTo": "2025-05-12T21:59:59.999Z",
-    "guests": 3,
-    "created": "2024-11-07T08:21:51.492Z",
-    "updated": "2024-11-07T08:21:51.492Z"
-  },
-  {
-    "id": "553f496e-dd37-4c41-9d74-e1ac393cc117",
-    "dateFrom": "2025-04-03T22:00:00.000Z",
-    "dateTo": "2025-04-12T21:59:59.999Z",
-    "guests": 1,
-    "created": "2024-11-07T08:21:51.492Z",
-    "updated": "2024-11-07T08:21:51.492Z"
-  },
-  {
-    "id": "566d27e0-8fae-42d5-a34d-5a92a4f37ccb",
-    "dateFrom": "2025-05-03T22:00:00.000Z",
-    "dateTo": "2025-05-12T21:59:59.999Z",
-    "guests": 3,
-    "created": "2024-11-07T08:21:51.492Z",
-    "updated": "2024-11-07T08:21:51.492Z"
-  }
-];
-
 function MyBookings() {
   const { name, accessToken, apiKey } = useUser(
     (state) => ({
@@ -59,7 +24,7 @@ function MyBookings() {
     },
   };
 
-  const { apiData, isLoading, isError } = VenueApi(profilesUrl + name + "/bookings", getOption);
+  const { apiData, isLoading, isError } = VenueApi(profilesUrl + "/" + name + "/bookings?_venue=true", getOption);
 
   if (isLoading || !apiData) {
     return <Loading />;
@@ -70,8 +35,7 @@ function MyBookings() {
   }
 
   console.log(apiData);
-  // return (apiData.length ? DisplayBookings(bookingsList) : <div>You have no bookings yet</div>);
-  return (bookingsList.length ? DisplayBookings(bookingsList) : <div>You have no bookings yet</div>);
+  return (apiData.length ? DisplayBookings(apiData) : <div>You have no bookings yet</div>);
 }
 
 export default MyBookings;
