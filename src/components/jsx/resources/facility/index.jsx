1ff26fab480f0a5d55526f3f18229dbc2b7d552a
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2024-12-28 21:27:40 +0100

    rearranged facility

--- src/components/jsx/resources/facility/index.jsx
+++ src/components/jsx/resources/facility/index.jsx
@@ -2,9 +2,9 @@ function Facility(facility) {
   return (
     <div className="my-2"> 
       <span className="facility me-1">{facility['breakfast'] ? "Breakfast":"No breakfast"}</span>
+      <span className="facility me-1">{facility['wifi'] ? "Free Wifi":"No Wifi"}</span>
       <span className="facility me-1">{facility['parking'] ? "Parking":"No parking"}</span>
       <span className="facility me-1">{facility['pets'] ? "Pet friendly":"No Pets"}</span>
-      <span className="facility me-1">{facility['wifi'] ? "Free Wifi":"No Wifi"}</span>
     </div>
   );
 }
