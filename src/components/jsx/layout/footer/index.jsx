0713120ba3e8a4a5170ea4ed40fddd03bdc21607
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2024-12-28 21:17:33 +0100

    hide login and register when logged in

--- src/components/jsx/layout/footer/index.jsx
+++ src/components/jsx/layout/footer/index.jsx
@@ -1,26 +1,35 @@
 import { Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import useUser from "../../store/user";
+import { shallow } from "zustand/shallow";
 
 function Footer() {
-    return (
-      <footer className="text-center py-3 mt-5">
-        <div>&#169; Holidaze 2025</div>
-        <ul className="footer-elements d-flex justify-content-between">
-            <li><Link className="footer-link ms-1" to="/">Home</Link></li>
-            <li><Link className="footer-link" to="/login">Login</Link></li>
-            <li><Link className="footer-link" to="/register">Register</Link></li>
-            <li><Link className="footer-link" to="/contact">Contact us</Link></li>
-            <li><Link className="footer-link me-1" to="/about">About us</Link></li>
-        </ul>
-        <div className="d-flex justify-content-center">
-          <p className="m-0">Follow us on:</p>
-          <Image src={require("../../../resources/socialMedia/facebook.png")} className="social-media mx-1" alt="facebook"/>
-          <Image src={require("../../../resources/socialMedia/tik-tok.png")} className="social-media mx-1" alt="tik-tok" fluid/>
-          <Image src={require("../../../resources/socialMedia/snapchat.png")} className="social-media mx-1" alt="snapchat"/>
-          <Image src={require("../../../resources/socialMedia/instagram.png")} className="social-media mx-1" alt="instagram"/>
-        </div>
-      </footer>
-    );
+  const { name } = useUser(
+    (state) => ({
+      name: state.name,
+    }),
+    shallow
+  );
+
+  return (
+    <footer className="text-center py-3 mt-5">
+      <div>&#169; Holidaze 2025</div>
+      <ul className="footer-elements d-flex justify-content-between">
+          <li><Link className="footer-link ms-1" to="/">Home</Link></li>
+          <li className={name ? "d-none" : "footer-link d-block"}><Link className="footer-link" to="/login">Login</Link></li>
+          <li className={name ? "d-none" : "footer-link d-block"}><Link className="footer-link" to="/register">Register</Link></li>
+          <li><Link className="footer-link" to="/contact">Contact us</Link></li>
+          <li><Link className="footer-link me-1" to="/about">About us</Link></li>
+      </ul>
+      <div className="d-flex justify-content-center">
+        <p className="m-0">Follow us on:</p>
+        <Image src={require("../../../resources/socialMedia/facebook.png")} className="social-media mx-1" alt="facebook"/>
+        <Image src={require("../../../resources/socialMedia/tik-tok.png")} className="social-media mx-1" alt="tik-tok" fluid/>
+        <Image src={require("../../../resources/socialMedia/snapchat.png")} className="social-media mx-1" alt="snapchat"/>
+        <Image src={require("../../../resources/socialMedia/instagram.png")} className="social-media mx-1" alt="instagram"/>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
