84d99f1a870a47ffe55adb4c40d85839a73ecdc2
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2024-12-28 17:59:19 +0100

    added custome default error message

--- src/components/js/bookingValidation/index.js
+++ src/components/js/bookingValidation/index.js
@@ -1,12 +1,21 @@
 import * as yup from "yup";
+import { setLocale } from "yup";
+
+setLocale({
+  number: {
+    min: "Deve ser maior que ${min}",
+    default: "cannot be empty",
+  },
+});
 
 const schema = yup
   .object({
     guests: yup
-      .number()
-      .required("Enter maximum number of guests")
-      .min(1)
-      .integer(),
+      .number("enter number of guests")
+      .min(1, "invalid")
+      .integer("enter number of guests")
+      .required("Enter number of guests")
+      .typeError("Enter number of guests"),
   })
   .required();
 
