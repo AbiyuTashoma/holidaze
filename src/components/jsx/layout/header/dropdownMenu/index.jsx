9b8e608872278c0b147492312563394c34dc12f4
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2025-01-01 11:02:19 +0100

    fixed link nesting warning

--- src/components/jsx/layout/header/dropdownMenu/index.jsx
+++ src/components/jsx/layout/header/dropdownMenu/index.jsx
@@ -1,5 +1,4 @@
 import { Dropdown } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import useUser from '../../../store/user';
 import { shallow } from 'zustand/shallow';
 
@@ -21,19 +20,19 @@ function NavMenu() {
       <span className={name? 'user d-block' : 'd-none'}>{name? name.slice(0,2).toUpperCase(): null}</span>
       <Dropdown.Toggle id="dropdown-basic">
         <svg width="40px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M4 18L20 18" stroke="#000000" strokeWidth="2" strokeLinecap="round"/>
-          <path d="M4 12L20 12" stroke="#000000" strokeWidth="2" strokeLinecap="round"/>
-          <path d="M4 6L20 6" stroke="#000000" strokeWidth="2" strokeLinecap="round"/>
+          <path d="M4 18L20 18" stroke="#FFFFFF" strokeWidth="2" strokeLinecap="round"/>
+          <path d="M4 12L20 12" stroke="#FFFFFF" strokeWidth="2" strokeLinecap="round"/>
+          <path d="M4 6L20 6" stroke="#FFFFFF" strokeWidth="2" strokeLinecap="round"/>
         </svg>
       </Dropdown.Toggle>
       <Dropdown.Menu align="end">
-        <Dropdown.Item className='p-0'><Link className='dropdown-item' to="/">Home</Link></Dropdown.Item>
-        <Dropdown.Item className={name? 'd-none':'p-0 d-block'}><Link className='dropdown-item' to="/login">Login</Link></Dropdown.Item>
-        <Dropdown.Item className={name? 'p-0 d-block':'d-none'}><Link className='dropdown-item' to="/profile">Profile</Link></Dropdown.Item>
-        <Dropdown.Item className={name? 'p-0 d-block':'d-none'} onClick={logout}><Link className='dropdown-item' to="/">Logout</Link></Dropdown.Item>
-        <Dropdown.Item className={name? 'd-none':'p-0 d-block'}><Link className='dropdown-item' to="/register">Register</Link></Dropdown.Item>
-        <Dropdown.Item className='p-0'><Link className='dropdown-item' to="/contact">Contact us</Link></Dropdown.Item>
-        <Dropdown.Item className='p-0'><Link className='dropdown-item' to="/about">About us</Link></Dropdown.Item>
+        <Dropdown.Item href="/">Home</Dropdown.Item>
+        <Dropdown.Item className={name? 'd-none':'d-block'} href="/login">Login</Dropdown.Item>
+        <Dropdown.Item className={name? 'd-block':'d-none'} href="/profile">Profile</Dropdown.Item>
+        <Dropdown.Item className={name? 'd-none':'d-block'} href="/register">Register</Dropdown.Item>
+        <Dropdown.Item href="/contact">Contact us</Dropdown.Item>
+        <Dropdown.Item href="/about">About us</Dropdown.Item>
+        <Dropdown.Item className={name? 'd-block':'d-none'} onClick={logout} href="/">Logout</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   )
