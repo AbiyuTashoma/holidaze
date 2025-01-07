243fe2203d88afaad6661dbd58e79ba29a8e992f
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2025-01-07 22:46:44 +0100

    Added register form unit test

--- src/components/jsx/resources/registerForm/index.jsx
+++ src/components/jsx/resources/registerForm/index.jsx
@@ -61,22 +61,22 @@ function RegisterForm () {
       <div>
         <label htmlFor="name" className="form-label">Username</label>
         <input id="name" name="name" className="form-control" {...register("name")} />
-        <p className="text-danger">{errors.name?.message}</p>
+        <p className="text-danger" data-testid="usernameError">{errors.name?.message}</p>
       </div>
       <div>
         <label htmlFor="email" className="form-label">Email</label>
         <input id="email" name="email" className="form-control" {...register("email")} />
-        <p className="text-danger">{errors.email?.message}</p>
+        <p className="text-danger" data-testid="emailError">{errors.email?.message}</p>
       </div>
       <div>
         <label htmlFor="password" className="form-label">Password</label>
         <input type="password" id="password" name="password" className="form-control" {...register("password")} />
-        <p className="text-danger">{errors.password?.message}</p>
+        <p className="text-danger" data-testid="passwordError">{errors.password?.message}</p>
       </div>
       <div>
         <label htmlFor="confirmPassword" className="form-label">Confirm password</label>
         <input type="password" id="confirmPassword" name="confirmPassword" className="form-control" {...register("confirmPassword")} />
-        <p className="text-danger">{errors.confirmPassword?.message}</p>
+        <p className="text-danger" data-testid="confirmError">{errors.confirmPassword?.message}</p>
       </div>
       <div>
         <label htmlFor="venueManager" className="form-label">Select a role</label>
@@ -85,12 +85,12 @@ function RegisterForm () {
           <option value={false}>User</option>
           <option value={true}>Venue Manager</option>
         </select>
-        <p className="text-danger">{errors.venueManager?.message}</p>
+        <p className="text-danger" data-testid="roleError">{errors.venueManager?.message}</p>
       </div>
       <div>
         <label htmlFor="avatar" className="form-label fst-italic">Avatar url (optional)</label>
         <input id="avatar" name="avatar" className="form-control" {...register("avatar")} />
-        <p className="text-danger">{errors.avatar?.message}</p>
+        <p className="text-danger" data-testid="avatarError">{errors.avatar?.message}</p>
       </div>
       <Button type="submit">Register</Button>
     </form>
