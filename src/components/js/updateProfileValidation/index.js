66f3622bbeacfe831299df405aec9185a335d93c
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2025-01-13 23:47:24 +0100

    improved edit profile feature

--- src/components/js/updateProfileValidation/index.js
+++ src/components/js/updateProfileValidation/index.js
@@ -11,6 +11,7 @@ const schema = yup
         /^(http(s)?:\/\/.)[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)$/,
         { message: "Enter a valid url" }
       ),
+    venueManager: yup.string().required("Select a role."),
   })
   .required();
 
